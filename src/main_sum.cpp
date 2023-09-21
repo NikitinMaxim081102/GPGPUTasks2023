d4b92fd6b4bacb7753b16e3dd8c254c29362c8a9
Maxon081102 <nikikitinm117@gmai.com>
2023-09-21 22:06:44 +0300

    done

--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -1,6 +1,10 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
+#include <libgpu/context.h>
 #include <libutils/fast_random.h>
+#include <libgpu/shared_device_buffer.h>
+
+#include "cl/sum_cl.h"
 
 
 template<typename T>
@@ -57,8 +61,139 @@ int main(int argc, char **argv)
         std::cout << "CPU OMP: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+    gpu::Context context;
+    context.init(device.device_id_opencl);
+    context.activate();
+    std::cout << std::endl;
+
+    gpu::gpu_mem_32u as_gpu, sum_gpu;
+    as_gpu.resizeN(n);
+    sum_gpu.resizeN(1);
+    
+    uint zero_sum = 0;
+    uint gpu_answer;
+
+    unsigned int workGroupSize = 64;
+    unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+
+    as_gpu.writeN(as.data(), n);
+    sum_gpu.writeN(&zero_sum, 1);
+
     {
-        // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        timer t;
+        ocl::Kernel sum_1(sum_kernel, sum_kernel_length, "sum_1");
+        sum_1.compile();
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            sum_gpu.writeN(&zero_sum, 1);
+            sum_1.exec(gpu::WorkSize(workGroupSize, global_work_size),
+                        as_gpu, sum_gpu, n);
+
+            sum_gpu.readN(&gpu_answer, 1);
+            EXPECT_THE_SAME(reference_sum, gpu_answer, "CPU result should be consistent!");
+            t.nextLap();
+        }
+        std::cout << "GPU sum_1:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU sum_1:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << std::endl;
     }
+
+    {
+        timer t;
+        ocl::Kernel sum_2(sum_kernel, sum_kernel_length, "sum_2");
+        sum_2.compile();
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            sum_gpu.writeN(&zero_sum, 1);
+            sum_2.exec(gpu::WorkSize(workGroupSize, global_work_size),
+                        as_gpu, sum_gpu, n);
+
+            sum_gpu.readN(&gpu_answer, 1);
+            EXPECT_THE_SAME(reference_sum, gpu_answer, "CPU result should be consistent!");
+            t.nextLap();
+        }
+        std::cout << "GPU sum_2:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU sum_2:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << std::endl;
+    }
+
+    {
+        timer t;
+        ocl::Kernel sum_1(sum_kernel, sum_kernel_length, "sum_3");
+        sum_1.compile();
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            sum_gpu.writeN(&zero_sum, 1);
+            sum_1.exec(gpu::WorkSize(workGroupSize, global_work_size),
+                        as_gpu, sum_gpu, n);
+
+            sum_gpu.readN(&gpu_answer, 1);
+            EXPECT_THE_SAME(reference_sum, gpu_answer, "CPU result should be consistent!");
+            t.nextLap();
+        }
+        std::cout << "GPU sum_3:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU sum_3:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << std::endl;
+    }
+
+    {
+        timer t;
+        ocl::Kernel sum_1(sum_kernel, sum_kernel_length, "sum_4");
+        sum_1.compile();
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            sum_gpu.writeN(&zero_sum, 1);
+            sum_1.exec(gpu::WorkSize(workGroupSize, global_work_size),
+                        as_gpu, sum_gpu, n);
+
+            sum_gpu.readN(&gpu_answer, 1);
+            EXPECT_THE_SAME(reference_sum, gpu_answer, "CPU result should be consistent!");
+            t.nextLap();
+        }
+        std::cout << "GPU sum_4:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU sum_4:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << std::endl;
+    }
+
+    {
+        timer t;
+        ocl::Kernel sum_1(sum_kernel, sum_kernel_length, "sum_5");
+        sum_1.compile();
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            sum_gpu.writeN(&zero_sum, 1);
+            sum_1.exec(gpu::WorkSize(workGroupSize, global_work_size),
+                        as_gpu, sum_gpu, n);
+
+            sum_gpu.readN(&gpu_answer, 1);
+            EXPECT_THE_SAME(reference_sum, gpu_answer, "CPU result should be consistent!");
+            t.nextLap();
+        }
+        std::cout << "GPU sum_5:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU sum_5:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << std::endl;
+    }
+
+    {
+        timer t;
+        gpu::gpu_mem_32u buff1, buff2;
+        buff1.resizeN(n);
+        buff2.resizeN(n);
+
+        ocl::Kernel sum_1(sum_kernel, sum_kernel_length, "sum_6");
+        sum_1.compile();
+
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            buff1.writeN(as.data(), n);
+            for (uint WORK_SIZE = n; WORK_SIZE > 1; WORK_SIZE = (WORK_SIZE + workGroupSize - 1) / workGroupSize) {
+                sum_1.exec(gpu::WorkSize(workGroupSize, WORK_SIZE),
+                        buff1, buff2, WORK_SIZE);
+                std::swap(buff1, buff2);
+            }
+
+            buff1.readN(&gpu_answer, 1);
+            EXPECT_THE_SAME(reference_sum, gpu_answer, "CPU result should be consistent!");
+            t.nextLap();
+        }
+        std::cout << "GPU sum_6:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU sum_6:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << std::endl;
+    }
+
 }
